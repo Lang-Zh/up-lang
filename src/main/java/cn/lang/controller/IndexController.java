29f970a949fbd426be5c97c057f92506434da067
lang <1102076808@qq.com>
2019-12-24 16:35:50 +0800

    外链域名配置化

--- src/main/java/cn/lang/controller/IndexController.java
+++ src/main/java/cn/lang/controller/IndexController.java
@@ -1,8 +1,14 @@
 package cn.lang.controller;
 
+import com.jfinal.kit.PropKit;
+
 public class IndexController extends BaseController{
 
     public void index() {
+        PropKit.use("qny.properties");
+        String domain = PropKit.get("domain");
+        System.err.println(domain);
+        setAttr("domain", domain);
         render("/lang/index.html");
     }
 }
